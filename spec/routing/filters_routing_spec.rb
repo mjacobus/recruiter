320f4cbb8186f5ccd5bd96081065891c9f280c59
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-06-17 14:50:00 -0300

    WIP [skip ci]

--- spec/routing/filters_routing_spec.rb
+++ spec/routing/filters_routing_spec.rb
@@ -1,8 +1,10 @@
 require "spec_helper"
 
 describe Recruiter::FiltersController do
+  routes { Recruiter::Engine.routes }
+
   it "routes to #index" do
-    expect(get('/filters')).to route_to('filters#index')
-    expect(get(filters_path)).to route_to('filters#index')
+    expect(get('/filters')).to route_to('recruiter/filters#index')
+    expect(get(filters_path)).to route_to('recruiter/filters#index')
   end
 end
--- spec/routing/jobs_routing_spec.rb
+++ spec/routing/jobs_routing_spec.rb
@@ -1,12 +1,14 @@
 require "spec_helper"
 
 describe Recruiter::JobsController, "routing" do
+  routes { Recruiter::Engine.routes }
+
   it "routes to #index" do
-    expect(get('/')).to route_to('jobs#index')
+    expect(get('/')).to route_to('recruiter/jobs#index')
   end
 
   it "routes to #show" do
-    expect(get('jobs/1')).to route_to('jobs#show', id: '1')
-    expect(get(offer_path(1))).to route_to('jobs#show', id: '1')
+    expect(get('jobs/1')).to route_to('recruiter/jobs#show', id: '1')
+    expect(get(offer_path(1))).to route_to('recruiter/jobs#show', id: '1')
   end
 end
