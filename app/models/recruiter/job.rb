935eccc5ea7365330663447dad89140c7caa115b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-06-18 14:23:46 -0300

    WIP

--- app/models/recruiter/job.rb
+++ app/models/recruiter/job.rb
@@ -2,7 +2,7 @@ module Recruiter
   class Job < ActiveRecord::Base
     belongs_to :user
     belongs_to :city
-    has_and_belongs_to_many :tags
+    has_and_belongs_to_many :tags, class_name: 'Recruiter::Tag'
 
     validates :user, presence: true
     validates :city, presence: true
@@ -10,7 +10,7 @@ module Recruiter
     validates :description, presence: true
     validates :how_to_apply, presence: true
 
-    default_scope ->{ order('jobs.created_at DESC') }
+    default_scope ->{ order('recruiter_jobs.created_at DESC') }
     scope :open, ->{ where(open: true) }
 
     def to_param
--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,35 +1,33 @@
 require 'machinist/active_record'
 
-Recruiter::User.blueprint do
-  name     { "The Name #{sn}" }
-  email    { "email#{sn}@example.com" }
-  provider { 'github' }
-  uid      { sn }
-end
+module Recruiter
+  User.blueprint do
+    name     { "The Name #{sn}" }
+    email    { "email#{sn}@example.com" }
+    provider { 'github' }
+    uid      { sn }
+  end
 
-Recruiter::Job.blueprint do
-  user
-  title { "ruby job #{sn}" }
-  description { "the requirements" }
-  how_to_apply { "how to apply" }
-  city
-end
+  Job.blueprint do
+    user
+    title { "ruby job #{sn}" }
+    description { "the requirements" }
+    how_to_apply { "how to apply" }
+    city
+  end
 
-Recruiter::State.blueprint do
-  name  { Faker::Address.city }
-  short { "#{sn}" }
-end
+  State.blueprint do
+    name  { Faker::Address.city }
+    short { "#{sn}" }
+  end
 
-Recruiter::City.blueprint do
-  name  { Faker::Address.city }
-  short { "#{sn}" }
-  state
-end
+  City.blueprint do
+    name  { Faker::Address.city }
+    short { "#{sn}" }
+    state
+  end
 
-Recruiter::Tag.blueprint do
-  name { "tag #{sn}" }
-end
-
-Recruiter::Tag.blueprint do
-  # Attributes here
+  Tag.blueprint do
+    name { "tag #{sn}" }
+  end
 end
