a45cd2c48d6c3fab866e27095142d7e59620821c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-06-20 16:02:53 -0300

    Fixed sign out

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Recruiter::Engine.routes.draw do
     controllers: { omniauth_callbacks: "recruiter/omniauth_callbacks" }
 
   devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
-    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'sign_in',  to: '/devise/sessions#new',     as: :new_user_session
+    get 'sign_out', to: '/devise/sessions#destroy', as: :destroy_user_session
   end
 
   scope :profile, module: :users do
