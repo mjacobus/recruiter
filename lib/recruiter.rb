9d8376928c280f075783ffea6ff25c8433ceb2f2
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-06-22 15:03:12 -0300

    Included foundation icons. Fixed #6

--- lib/recruiter.rb
+++ lib/recruiter.rb
@@ -7,7 +7,7 @@ require "kaminari"
 require "draper"
 require "simple_form"
 require "foundation-rails"
-# require "foundation-icons-sass-rails"
+require "foundation-icons-sass-rails"
 require "jquery-rails"
 
 require "recruiter/engine"
